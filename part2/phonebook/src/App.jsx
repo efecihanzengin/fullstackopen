2c1bd8c709e7e29bd3d0dde54b0929bed68c496f
Efe Cihan Zengin <efecihanzengin@gmail.com>
2026-01-21 15:47:01 +0300

    Refactor App component for improved readability and consistency in formatting

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,81 +5,81 @@ import Persons from "./components/Persons";
 import personService from './services/persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([]);
-    const [newName, setNewName] = useState("");
-    const [newNumber, setNewNumber] = useState("");
-    const [filterWord, setFilterWord] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterWord, setFilterWord] = useState("");
 
-    useEffect(() => {
-        personService
-            .getAll()
-            .then(initialPerson => {
-                setPersons(initialPerson);
-            });
-    }, []);
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPerson => {
+        setPersons(initialPerson);
+      });
+  }, []);
 
-    const addPerson = (e) => {
-        e.preventDefault();
-        if (persons.find((person) => person.name === newName)) {
-            alert(`${newName} is already added to phonebook`);
-        } else {
-            const nameObject = {
-                name: newName,
-                id: persons.length + 1,
-                number: newNumber,
-            };
-            personService
-                .create(nameObject)
-                .then(initialPerson => {
-                    setPersons(persons.concat(initialPerson))
-                    setNewName("");
-                })
-        }
-    };
-
-    const deletePerson = (id) => {
-        personService
-            .deletePerson(id)
-            .then(() => {
-                setPersons(persons.filter(n => n.id !== id))
-            })
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const nameObject = {
+        name: newName,
+        id: persons.length + 1,
+        number: newNumber,
+      };
+      personService
+        .create(nameObject)
+        .then(initialPerson => {
+          setPersons(persons.concat(initialPerson))
+          setNewName("");
+        })
     }
+  };
 
-    const handlePersonChange = (e) => {
-        console.log(e.target.value);
-        setNewName(e.target.value);
-    };
+  const deletePerson = (id) => {
+    personService
+      .deletePerson(id)
+      .then(() => {
+        setPersons(persons.filter(n => n.id !== id))
+      })
+  }
 
-    const handleNumberChange = (e) => {
-        setNewNumber(e.target.value);
-    };
+  const handlePersonChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
 
-    const handleFilterChange = (e) => {
-        setFilterWord(e.target.value);
-    };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <SearchFilter value={filterWord} onChange={handleFilterChange} />
+  const handleFilterChange = (e) => {
+    setFilterWord(e.target.value);
+  };
 
-            <h3>Add new</h3>
-            <PersonForm
-                addPerson={addPerson}
-                newName={newName}
-                handlePersonChange={handlePersonChange}
-                newNumber={newNumber}
-                handleNumberChange={handleNumberChange}
-            />
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <SearchFilter value={filterWord} onChange={handleFilterChange} />
 
-            <h2>Numbers</h2>
+      <h3>Add new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
-            <Persons
-                persons={persons}
-                filterWord={filterWord}
-                deletePerson={deletePerson} />
-        </div>
-    );
+      <h2>Numbers</h2>
+
+      <Persons
+        persons={persons}
+        filterWord={filterWord}
+        deletePerson={deletePerson} />
+    </div>
+  );
 };
 
 export default App;
