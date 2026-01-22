fd4ca41d4e9df556bef5be378622bfea62d05124
Efe Cihan Zengin <efecihanzengin@gmail.com>
2026-01-22 12:14:42 +0300

    Refactor toggleImportanceOf function to use note ID directly in update call

--- part2/sandbox/src/App.jsx
+++ part2/sandbox/src/App.jsx
@@ -38,12 +38,11 @@ const App = () => {
   const toggleImportanceOf = (id) => {
     console.log(`importance of ${id} needs to be toggled`);
 
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
     noteService
-      .update(url, changedNote)
+      .update(id, changedNote)
       .then((returnedNote) => {
         setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
       })
